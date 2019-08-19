85e600bf1b471e1244f945c386dcecca1407f394
Gabriel Bourg <s198007@students.aie.edu>
2019-08-19 17:39:23 -0500

    Add project files.

---
+++ functions/Game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functions
+{
+    class Game
+    {
+        int numberone = 10;
+        int numbertwo = 200;
+        public void start()
+        {
+            int biggestnumber = numbertwo;
+
+            Console.WriteLine("of " + numberone + " and " + numbertwo + " the biggest number is " + biggestnumber);
+            Console.ReadKey();
+        }
+        int ReturnBiggestNumber(int first,int second)
+        {
+            if(first > second)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+    }
+}
